f958c4544b4b6382d59c62bd3d29efd7ed4b4237
hiteshdullu <hiteshdullu@gmail.com>
2025-01-12 16:08:01 +0530

    initial commit

---
+++ restaurant.py
@@ -0,0 +1,8 @@
+class Restaurant:
+    def __init__(self, name, city, menu, status, location):
+        self.name = name
+        self.city = city
+        self.menu = menu
+        self.status = status
+
+    
